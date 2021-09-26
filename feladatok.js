5e318fabae2172060c066beea8b8d051f5b2d663
BartuczPetra <bartucz.petra@gmail.com>
2021-09-26 18:35:14 +0200

    javitasok

--- feladatok.js
+++ feladatok.js
@@ -1,7 +1,7 @@
 $(function () {
   //Niki
-  var osztalyTomb = [];
-  var allatokTomb = [];
+  const osztalyTomb = [];
+  const allatokTomb = [];
   let valasztott;
   let i;
   //console.log($('option:selected').index());
@@ -15,17 +15,24 @@ $(function () {
     valasztott = "osztaly3";
     i = 3;
   }
+  console.log($("option:selected").index());
 
   $.ajax({
     url: valasztott + ".json",
     success: function (result) {
       console.log(result);
       if ((valasztott = "osztaly1")) {
-        osztalyTomb = result.osztaly1;
+		for (let i = 0; i < result.osztaly1.length; i++) {
+			osztalyTomb.push(result.osztaly1[i]);
+			}
       } else if ((valasztott = "osztaly2")) {
-        osztalyTomb = result.osztaly2;
+        for (let i = 0; i < result.osztaly2.length; i++) {
+			osztalyTomb.push(result.osztaly2[i]);
+			}
       } else if ((valasztott = "osztaly3")) {
-        osztalyTomb = result.osztaly3;
+        for (let i = 0; i < result.osztaly3.length; i++) {
+			osztalyTomb.push(result.osztaly3[i]);
+			}
       }
       console.log(osztalyTomb);
     },
@@ -34,17 +41,18 @@ $(function () {
   $.ajax({
     url: "adatok.json",
     success: function (result) {
-      console.log(result);
-      allatokTomb = result.allatlista;
+	  console.log(result);
+	  for (let i = 0; i < result.allatlista.length; i++) {
+		allatokTomb.push(result.allatlista[i]);
+		}
       console.log(allatokTomb);
     },
   });
 
   //Petra
 
-  $("#nyomtat").click(
-    () => {
-      console.log("valami");
+  $("#nyomtat").click(() => {
+	  $("section").empty();
       $("section").append("<table></table>");
 
       osztalyTomb.forEach((value, index) => {
